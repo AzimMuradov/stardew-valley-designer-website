0d33276f33b0e36f47d34588256b0dbdb12cdc3c
Azim Muradov <azim.muradov.dev@gmail.com>
2024-06-29 18:39:31 +0300

    Import latest website code

--- editor/svd.js
+++ editor/svd.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dc019a2695a79eaa92c5315e2df7da4b57748ed1026e6d792bdd7f5ac842555d
-size 551129
+oid sha256:81e7370fe4cb98b3cebb7dc17bd540bc426cf5917d4e70f37d6dda5f20b36b70
+size 551119
